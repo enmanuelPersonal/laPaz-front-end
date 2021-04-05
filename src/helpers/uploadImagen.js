478256d71bad23c54fad9b7b63c117f50346241c
jesus07098 <jesus07098@gmail.com>
2021-04-05 01:05:47 -0400

    Resolve bug upload image

--- src/helpers/uploadImagen.js
+++ src/helpers/uploadImagen.js
@@ -1,15 +1,32 @@
-import { post } from "./fetch";
+import { post } from './fetch';
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+const { REACT_APP_API_URL } = process.env;
 
 export const uploadImagen = async (imagen) => {
-     console.log("Estoy aqui", imagen[0]);
-     const formData = new FormData();
-     formData.append('image', imagen[0]);
-     formData.append('nombre', 'Enmanuel');
-     // for (var key of formData.entries()) {
-     //      console.log(key[0] + ', ' + key[1]);
-     //  }
-     const res = await post('img',formData).then(res => res.json());
+  const token = localStorage.getItem('LaPaz_auth_token');
 
-     console.log("============> ", res);
-   };
-   
+  console.log('Estoy aqui', imagen[0]);
+  const formData = new FormData();
+  formData.append('image', imagen[0]);
+  formData.append('nombre', 'Enmanuel');
+  const bodyOpts = {
+    method: 'POST',
+    headers: {
+      LaPaz_auth_token: token,
+      credentials: 'include',
+    },
+    body: formData,
+  };
+
+  // for (var key of formData.entries()) {
+  //      console.log(key[0] + ', ' + key[1]);
+  //  }
+  const res = await fetch(`${REACT_APP_API_URL}img`, bodyOpts).then((res) =>
+    res.json()
+  );
+
+  console.log('============> ', res);
+};
